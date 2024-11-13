321e805d3af1c3682db9b94e024d03a654085c28
mucnjakf <filip.mucnjak@outlook.com>
2024-11-14 00:38:25 +0100

    Refactored technical skills to components

---
+++ Portfolio.Web/Dtos/SkillDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Portfolio.Web.Dtos;
+
+public sealed class SkillDto
+{
+    public string Src { get; set; } = default!;
+
+    public string Alt { get; set; } = default!;
+
+    public string Text { get; set; } = default!;
+}
---
+++ Portfolio.Web/Dtos/TechnicalSkillsDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Portfolio.Web.Dtos;
+
+public sealed class TechnicalSkillsDto
+{
+    public IList<SkillDto> Skills { get; set; } = [];
+
+    public IEnumerable<string> Architecture { get; set; } = [];
+    
+    public IEnumerable<string> Pattern { get; set; } = [];
+    
+    public IEnumerable<string> Methodology { get; set; } = [];
+    
+    public IEnumerable<string> Other { get; set; } = [];
+}
