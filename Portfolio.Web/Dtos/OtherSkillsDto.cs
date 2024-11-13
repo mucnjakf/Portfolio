4a141d876b9afd90b7346e691c9f14df88724b0f
mucnjakf <filip.mucnjak@outlook.com>
2024-11-14 00:43:59 +0100

    Fixed other skills

--- Portfolio.Web/Dtos/OtherSkillsDto.cs
+++ Portfolio.Web/Dtos/OtherSkillsDto.cs
@@ -1,7 +1,9 @@
 ﻿namespace Portfolio.Web.Dtos;
 
-public sealed class LanguageSkillsDto
+public sealed class OtherSkillsDto
 {
+    public IEnumerable<string> SoftSkills { get; set; } = [];
+    
     public string MotherTongue { get; set; } = default!;
 
     public IEnumerable<LanguageDto> Languages { get; set; } = [];
