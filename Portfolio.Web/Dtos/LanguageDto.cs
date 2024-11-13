9d334a3aa777f86e1b24fc290cf9a57ea9972955
mucnjakf <filip.mucnjak@outlook.com>
2024-11-14 00:11:56 +0100

    Refactored other skills to components

---
+++ Portfolio.Web/Dtos/LanguageDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Portfolio.Web.Dtos;
+
+public sealed class LanguageDto
+{
+    public string Name { get; set; } = default!;
+    
+    public string Listening { get; set; } = default!;
+    
+    public string Reading { get; set; } = default!;
+    
+    public string SpokenProduction { get; set; } = default!;
+    
+    public string SpokenInteraction { get; set; } = default!;
+    
+    public string Writing { get; set; } = default!;
+}
---
+++ Portfolio.Web/Dtos/LanguageSkillsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Portfolio.Web.Dtos;
+
+public sealed class LanguageSkillsDto
+{
+    public string MotherTongue { get; set; } = default!;
+
+    public IEnumerable<LanguageDto> Languages { get; set; } = [];
+}
