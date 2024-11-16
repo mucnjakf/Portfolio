d5f2d52043544fba8337762e9359688e993925a1
mucnjakf <filip.mucnjak@outlook.com>
2024-11-16 03:05:12 +0100

    Implemented personal projects

---
+++ Portfolio.Web/Dtos/PersonalProjectsDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Portfolio.Web.Dtos;
+
+public sealed class PersonalProjectsDto
+{
+    public string Name { get; set; } = default!;
+
+    public string Image { get; set; } = default!;
+
+    public string Description { get; set; } = default!;
+
+    public IEnumerable<string> TechStack { get; set; } = [];
+
+    public string Link { get; set; } = default!;
+}
