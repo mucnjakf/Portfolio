a55badd25cf3e7a8d4806fb4b1ce2ee9f278cf36
mucnjakf <filip.mucnjak@outlook.com>
2024-11-24 16:14:23 +0100

    Refactored skills

--- Portfolio.Web/Dtos/TechnicalSkillsDto.cs
+++ Portfolio.Web/Dtos/TechnicalSkillsDto.cs
@@ -1,23 +1,18 @@
-﻿namespace Portfolio.Web.Dtos;
+﻿using System.Collections;
+
+namespace Portfolio.Web.Dtos;
 
 public sealed class TechnicalSkillsDto
 {
-    public IList<SkillDto> Skills { get; set; } = [];
+    public IList<string> Skills { get; set; } = [];
+
+    public IEnumerable<string> ProgrammingLanguages { get; set; } = [];
 
     public IEnumerable<string> Architecture { get; set; } = [];
-    
+
     public IEnumerable<string> Pattern { get; set; } = [];
-    
+
     public IEnumerable<string> Methodology { get; set; } = [];
-    
+
     public IEnumerable<string> Other { get; set; } = [];
-}
-
-public sealed class SkillDto
-{
-    public string Src { get; set; } = default!;
-
-    public string Alt { get; set; } = default!;
-
-    public string Text { get; set; } = default!;
 }
