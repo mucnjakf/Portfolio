994fe76de510cb688cb49752c2a1ec2bbea54515
mucnjakf <filip.mucnjak@outlook.com>
2024-11-25 23:32:17 +0100

    Refactored technical skills

--- Portfolio.Web/Dtos/TechnicalSkillsDto.cs
+++ Portfolio.Web/Dtos/TechnicalSkillsDto.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
-
-namespace Portfolio.Web.Dtos;
+﻿namespace Portfolio.Web.Dtos;
 
 public sealed class TechnicalSkillsDto
 {
-    public IList<string> Skills { get; set; } = [];
-
-    public IEnumerable<string> ProgrammingLanguages { get; set; } = [];
-
+    public IEnumerable<string> Languages { get; set; } = [];
+    
+    public IEnumerable<string> Backend { get; set; } = [];
+    
+    public IEnumerable<string> Frontend { get; set; } = [];
+    
+    public IEnumerable<string> Testing { get; set; } = [];
+    
+    public IEnumerable<string> Database { get; set; } = [];
+    
+    public IEnumerable<string> VersionControl { get; set; } = [];
+    
     public IEnumerable<string> Architecture { get; set; } = [];
 
-    public IEnumerable<string> Pattern { get; set; } = [];
+    public IEnumerable<string> DesignPatterns { get; set; } = [];
+    
+    public IEnumerable<string> DesignPrinciples { get; set; } = [];
 
-    public IEnumerable<string> Methodology { get; set; } = [];
-
-    public IEnumerable<string> Other { get; set; } = [];
+    public IEnumerable<string> CICD { get; set; } = [];
+    
+    public IEnumerable<string> Methodologies { get; set; } = [];
+    
+    public IEnumerable<string> DevelopmentTools { get; set; } = [];
+    
+    public IEnumerable<string> ProjectManagementTools { get; set; } = [];
+    
+    public IEnumerable<string> ProductivityTools { get; set; } = [];
+    
+    public IEnumerable<string> OperatingSystems { get; set; } = [];
 }
