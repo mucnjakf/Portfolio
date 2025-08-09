f5cbfa57ac4a6eae94d70039a94974171c6d60fd
mucnjakf <crofilix@gmail.com>
2025-08-09 21:52:47 +0200

    Updated profile

--- Portfolio.Web/Dtos/ProfileDto.cs
+++ Portfolio.Web/Dtos/ProfileDto.cs
@@ -2,25 +2,27 @@
 
 public sealed class ProfileDto
 {
-    public string ProfileImage { get; init; } = default!;
+	public string ProfileImage { get; init; } = default!;
 
-    public string Name { get; init; } = default!;
+	public string Name { get; init; } = default!;
 
-    public string Description { get; init; } = default!;
+	public string Description { get; init; } = default!;
 
-    public string Professional { get; init; } = default!;
+	public string Professional { get; init; } = default!;
 
-    public string Education { get; init; } = default!;
+	public string Education { get; init; } = default!;
 
-    public string Gender { get; init; } = default!;
+	public string Gender { get; init; } = default!;
 
-    public string DateOfBirth { get; init; } = default!;
+	public string DateOfBirth { get; init; } = default!;
 
-    public string Location { get; init; } = default!;
+	public string Location { get; init; } = default!;
 
-    public string Email { get; init; } = default!;
+	public string Email { get; init; } = default!;
 
-    public string GitHub { get; init; } = default!;
+	public string GitHub { get; init; } = default!;
 
-    public string MyStory { get; init; } = default!;
+	public string LinkedIn { get; init; } = default!;
+
+	public string MyStory { get; init; } = default!;
 }
