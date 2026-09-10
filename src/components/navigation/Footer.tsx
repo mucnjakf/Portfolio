9057c86419e6966d3f514c3a4ed3f45664f66135
mucnjakf <crofilix@gmail.com>
2026-09-10 18:08:33 +0200

    Remove NavBrand component from Footer for cleaner layout

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -2,14 +2,12 @@ import IconLinkFlat from "../button/IconLinkFlat";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 import { LuPhone } from "react-icons/lu";
-import NavBrand from "./NavBrand.tsx";
 
 function Footer() {
   return (
     <footer className="w-full bg-zinc-100 dark:bg-zinc-900">
       <div className="mx-auto flex w-full max-w-[1300px] items-center justify-between px-4 py-5 min-[650px]:px-10">
         <div className="flex items-center text-sm font-medium text-zinc-400 dark:text-zinc-600">
-          <NavBrand />
           &copy; {new Date().getFullYear()}.
         </div>
 
